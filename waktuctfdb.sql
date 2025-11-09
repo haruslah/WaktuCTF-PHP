6596d75540b719aec5cc95f160467742b5734734
haruslah <coksad515@gmail.com>
2025-11-09 16:54:00 +0800

    Modify SQL dump with new entries and adjustments
    Updated SQL dump with new data and structure changes.

--- waktuctfdb.sql
+++ waktuctfdb.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 02, 2025 at 12:22 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost
+-- Generation Time: Nov 09, 2025 at 06:49 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `ctf_events` (
   `ctf_id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `date` date NOT NULL,
-  `format` varchar(50) NOT NULL,
+  `format` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -39,8 +39,9 @@ CREATE TABLE `ctf_events` (
 --
 
 INSERT INTO `ctf_events` (`ctf_id`, `title`, `date`, `format`) VALUES
-(1, 'HackThePlanet', '2025-12-01', 'Jeopardy'),
-(2, 'WebWarriors', '2025-12-10', 'Attack/Defense');
+(5, 'Stacksmash CTF', '2025-11-16', 'Jeopardy'),
+(6, 'CyberDefense CTF', '2025-11-22', 'Attack-Defense'),
+(7, 'TryH4ck CTF', '2025-10-02', 'Jeopardy');
 
 -- --------------------------------------------------------
 
@@ -60,7 +61,7 @@ CREATE TABLE `teams` (
 --
 
 INSERT INTO `teams` (`team_id`, `team_name`, `created_by`, `created_at`) VALUES
-(1, 'pisang', 7, '2025-11-02 18:46:48');
+(5, 'puasa6', 13, '2025-11-09 13:30:50');
 
 -- --------------------------------------------------------
 
@@ -80,7 +81,8 @@ CREATE TABLE `team_members` (
 --
 
 INSERT INTO `team_members` (`id`, `team_id`, `user_id`, `joined_at`) VALUES
-(1, 1, 7, '2025-11-02 18:46:48');
+(2, 5, 13, '2025-11-09 13:30:50'),
+(3, 5, 12, '2025-11-09 13:35:09');
 
 -- --------------------------------------------------------
 
@@ -94,20 +96,17 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password_hash` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `ctf_joined` int(11) NOT NULL
+  `ctf_joined` int(11) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password_hash`, `created_at`, `ctf_joined`) VALUES
-(1, 'admin', 'admin@gmail.com', '$2y$10$m9mabPuvmf.1Wrrw0Vp0ue1UZt8HG9OEHVcJfzvhzwW03MpWckJ3y', '2025-10-21 15:49:22', 0),
-(3, 'test1', 'test1@gmail.com', '$2y$10$t2Kc3RKFdubhuF20td3.puXhj3SsxRISYML2SA542He9vi5CxF2Ee', '2025-10-27 13:03:09', 0),
-(4, 'test2', 'test2@gmail.com', '$2y$10$Y5sf/bS2vVeS/UC82vZr1ugjSWaQWf8Tmjl0dmNI/C9nsEOFIuMEu', '2025-10-27 13:22:23', 0),
-(5, 'test3', 'test3@gmail.com', '$2y$10$L9DMPZFtHE/tJ53U4B9dAemnszmpZwgtGFdQDxhuVHsZL9XDaFI/.', '2025-10-27 13:22:46', 0),
-(6, 'test4', 'test4@gmail.com', '$2y$10$wVhL1xFGLVdKZHAMjjfBMed57YouLknSda3/nzKvBQpLIvgcw3Blm', '2025-10-27 13:32:38', 0),
-(7, 'test5', 'test5@gmail.com', '$2y$10$ceZLy9Tv3chEYpZChiqm5uCXFny8TcEe1TDvWI16GIg6lv4gkN.rC', '2025-11-02 10:08:36', 1);
+INSERT INTO `users` (`id`, `username`, `email`, `password_hash`, `created_at`, `ctf_joined`, `is_admin`) VALUES
+(12, 'admin', 'admin@gmail.com', '$2y$10$YqUk2JYlnXYsjPd9y22rg.DvDpxfy1r37js01b2W0KpN5WQjYqa9S', '2025-11-09 02:54:05', 1, 1),
+(13, 'user', 'user123@gmail.com', '$2y$10$OV56PmQqKtxynoxTB1pc2.c4W8KqSum.iabPwyMvQXkNLI3dV6UvW', '2025-11-09 04:44:03', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -131,7 +130,7 @@ CREATE TABLE `user_ctf` (
 --
 
 INSERT INTO `user_ctf` (`id`, `user_id`, `ctf_id`, `team_name`, `experience`, `notify`, `notes`, `joined_at`) VALUES
-(1, 7, 1, 'pisang', 'beginner', 'Email', 'aa', '2025-11-02 19:19:18');
+(1, 12, 6, 'puasa6', 'advanced', 'Discord', 'I\'ll be playing solo.', '2025-11-09 13:43:25');
 
 --
 -- Indexes for dumped tables
@@ -183,25 +182,25 @@ ALTER TABLE `user_ctf`
 -- AUTO_INCREMENT for table `ctf_events`
 --
 ALTER TABLE `ctf_events`
-  MODIFY `ctf_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ctf_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `teams`
 --
 ALTER TABLE `teams`
-  MODIFY `team_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `team_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `team_members`
 --
 ALTER TABLE `team_members`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `user_ctf`
